e776393ade136375c96371104333849714f05443
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-15 21:53:59 -0700

    add link to create new list and edit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,35 @@
 class ListsController < ApplicationController
+  def new
+    @list = List.new
+    render :new
+  end
+
+  def create
+    @list = List.new(params[:list])
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(params[:list])
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
   def index
     @lists = List.all
     render :index
+  end
+
+  def edit
+    @list = List.find(params[:id])
+    render :edit
   end
 
   def show
